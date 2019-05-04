3900a351ba92fa855b269ea8702b6773f4f29c56
unknown <oscar.charo09@gmail.com>
2019-05-03 21:12:02 -0500

    npm required and api keys

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
-
+const axios = require('axios');
 const keys = require("./keys.js");
-
+const inquirer = require("inquirer");
+var Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
