d674489ae40040be01c2663043208a39a6da10e3
unknown <oscar.charo09@gmail.com>
2019-05-03 22:18:09 -0500

    choises & music funtion

--- liri.js
+++ liri.js
@@ -5,9 +5,31 @@ const inquirer = require("inquirer");
 var Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
 
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Would you like to search for a concert, movie, or event?",
+            choices: ["events", "music", "movies"],
+            name: "type"
+        },
+        {
+            type: "search",
+            message: "What exactly are you searching for?",
+            name: "search"
+        }
+    ])
+    .then(function (response) {
+        if (response.type === "Movies"){
+            omdb(response.search)
+        } else if (response.type === "Events"){
+            tm(response.search)
+        } else music(response.search)
+    });
 
 
-    const options = {
+
+    const tm = {
         provider: "ticketmasrter",
         apiKey: "TICKETMASTER_API_KEY"
       };
@@ -24,6 +46,21 @@ const spotify = new Spotify(keys.spotify);
                 console.log(error);
     });
 console.log(options);
+
+    function music(search){
+        spotify.search({ type: 'track', query: search, limit: 1 }, function(err, data) {
+            if (err) {
+            return console.log('Error occurred: ' + err);
+            }
+        
+        console.log("Artist: " + data.tracks.items[0].artists[0].name); 
+        console.log("song Name: " + data.tracks.items[0].songname);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url); 
+        console.log("Album: " + data.tracks.items[0].album.name); 
+        
+      });
+}
+
    
 
  
